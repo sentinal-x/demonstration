771f87bcb43ec4d365af4c14030c222539d3a2a3
Simon James Puttock <simonjputtock@gmail.com>
2023-03-02 15:09:26 +0000

    First recursive commit

---
+++ factorial/factorial_test.go
@@ -0,0 +1,27 @@
+package factorial
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestUnitFactorial(t *testing.T) {
+	assert := assert.New(t)
+	tests :=
+		[]struct {
+			n      int
+			result int
+		}{
+			{0, 1},
+			{1, 1},
+			{2, 2},
+			{4, 24},
+		}
+
+	for _, test := range tests {
+		/* act */
+		v := Factorial(test.n)
+		/* assert */
+		assert.Equal(test.result, v)
+	}
+}
